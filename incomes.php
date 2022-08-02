932101ec248fa555090def26e848a86452bce44d
Sandra Skibiszewska <sandra.skibiszewska.programista@gmail.com>
2022-08-02 11:24:02 +0200

    created added_income page

--- incomes.php
+++ incomes.php
@@ -1,3 +1,52 @@
+<?php
+
+    session_start();
+
+    if(isset($_POST['amount']))
+    {
+        //Udana walidacja
+        $everything_ok = true;
+
+        $income_category_assigned_to_user_id = $_POST['category'];
+        $amount = $_POST['amount'];
+        $date_of_income = $_POST['date'];
+      
+        //Sprawdzenie dlugosci komentarza
+        $income_comment = $_POST['comment'];
+        if (strlen($income_comment) > 100){
+            $everything_ok = false;
+            $_SESSION['e_income_comment'] = "The maximum length of a comment is 100 characters!";
+        }
+
+
+        require_once "connect.php";
+        mysqli_report(MYSQLI_REPORT_STRICT);
+
+        try{
+            $connection = new mysqli($host, $db_user, $db_password, $db_name); 
+            if ($connection->connect_errno!=0){
+                throw new Exception (mysqli_connect_errno());
+            }
+            
+            if ($everything_ok == true){
+                //Wszystkie testy zaliczone
+                if ($connection->query("INSERT INTO incomes VALUES (NULL, 1, '$income_category_assigned_to_user_id', ' $amount', ' $amount', ' $income_comment' )")){
+                    $_SESSION['addedincome'] = true;
+                    header('Location: added_income.php');
+                }
+                else{
+                    throw new Exception ($connection->error);
+                }
+            }
+            $connection->close();
+            }
+        catch(Exception $e){
+            echo 'Blad serwera!';
+          //  echo '<br/>Informacja developerska: '.$e;
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +55,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Personal Budget - Add Income</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="incomess.css" type="text/css">
+    <link rel="stylesheet" href="incomee.css" type="text/css">
     <link rel="stylesheet" href="css/fontello.css" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Libre+Bodoni:ital,wght@1,500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
@@ -25,10 +74,10 @@
                 <h2 class="logo">Add Income</h2>
             </header>
             <div class="row">
-                <form>
+                <form method = "post">
                 <p>
-                        <label class="category">Category </label>
-                        <select id="income">
+                        <label class="categories">Category </label>
+                        <select id="income" name ="category">
                             <option>salary</option>
                             <option>bank interest</option>
                             <option>sale on allegro</option>
@@ -36,17 +85,33 @@
                         </select>
                     </p>
                     <p>
-                        <label class="amount">Amount <input type="number" placeholder="123" step="0.01" min="0"></label>
+                        <label class="amount">Amount </label> 
+                        <input type="number" name="amount" placeholder="123" step="0.01" min="0">
+                        <?php
+
+                            if (isset($_SESSION['e_amount'])){
+                                echo '<div class="error">'.$_SESSION['e_amount'].'</div>';
+                                unset($_SESSION['e_amount']);
+                            }
+                        ?>
                     </p>
                     <p>
-                        <label class="date" for="start">Date </label>
-                        <input type="date" id="start" name="trip-start">
+                        <label class="date" for="start">Date </label> 
+                        <input type="date" id="start" name="date">
                     </p>
                     <p>
-                        <label class="comment">Comment <input type="text" placeholder="(optional)"></label>
+                        <label class="comment">Comment </label>
+                        <input type="text"  name="comment" placeholder="(optional)">
+                        <?php
+
+                            if (isset($_SESSION['e_comment'])){
+                                echo '<div class="error">'.$_SESSION['e_comment'].'</div>';
+                                unset($_SESSION['e_income_comment']);
+                            }
+                        ?>
                     </p>
                     <input id="add" type="submit" value="Add">
-                    <input type="submit" value="Cancel">
+                    <a href="main_menu.php"><input type="button" value="Cancel"></a>  
                 </form>
             </div>
         </article>
